cdf0f9e63d249b4299cf91e32e73da362ba233f0
Shiva Patel <patelshiva1641@gmail.com>
2026-06-18 19:57:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 //  3. Open index.html with Live Server or python -m http.server 3000
 // ─────────────────────────────────────────────────────────
 
-const GROQ_API_KEY = 'YOUR_GROQ_API_KEY_HERE';
+const GROQ_API_KEY = 'gsk_xb2sVKpFTQE61YKJWDpfWGdyb 3FYjhJmatk08KC2FxTun2icgAtV';
 const GROQ_MODEL   = 'llama-3.3-70b-versatile';   // free & fast
 const GROQ_URL     = 'https://api.groq.com/openai/v1/chat/completions';
 
